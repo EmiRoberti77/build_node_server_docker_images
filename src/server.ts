ba32674512762bd9f07e4373c13055f7a6df96c0
EmiRoberti77 <emiliano.roberti@accenture.com>
2025-02-06 16:27:05 +0000

    corrected message

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import express, { Request, Response } from "express";
-import Cors from "cors";
+import express, { Request, Response } from 'express';
+import Cors from 'cors';
 
 const app = express();
 app.use(express.json());
 app.use(Cors());
 
-app.get("/api", (req: Request, res: Response) => {
+app.get('/api', (req: Request, res: Response) => {
   res.status(200).json({
-    messsage: "server up " + new Date().toISOString(),
+    message: 'server up ' + new Date().toISOString(),
   });
 });
 
 app.listen(8090, () => {
-  console.log("server listening on 8090");
+  console.log('server listening on 8090');
 });
